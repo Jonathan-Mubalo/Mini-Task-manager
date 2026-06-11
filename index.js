db81ea6972d6632724983020f9b8d6889c6b951d
ace2921 <austinmwelwa44@gmail.com>
2026-06-11 14:59:25 +0200

    Added the drad effect and removed buttons

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const body = document.querySelector("body")
 //  MY ARRAY TO DISPLAY IN ANY CONTAINER THAT IS MADE
 
 
-let boilerplate = ["<button>Finish Editting</button>", "<h6>Move to To-Do</h6>", "<h3>Move to Doing</h3>", "<h4>Move to Done</h4>", "<h2>Mark as Done</h2>", "<h5>Delete Task</h5>"]
+let boilerplate = ["<button>Finish Editting</button>", "<h2>Mark as Done</h2>", "<h5>Delete Task</h5>"];
+//  "<h6>Move to To-Do</h6>", "<h3>Move to Doing</h3>", "<h4>Move to Done</h4>",
 
 // "<h5>Edit task</h5>",
 //FUNCTION USED TO CREATE A TASK CARD AND ADD IT TO THE TODO DIV
+
+
+let myIdCount = 0;
 
 createBtn.addEventListener("click", () => {
     if (addTaskInput.value === "") {
@@ -22,7 +26,12 @@ createBtn.addEventListener("click", () => {
     let cardTitle = document.createElement("article");
     let editTaskSpan = document.createElement("input");
     editTaskSpan.placeholder = "Click To Edit Task";
+    mySectionCard.id = "a"+myIdCount;
+    mySectionCard.draggable ="true";
+    parseInt(myIdCount);
+    myIdCount++;
     cardTitle.innerHTML = addTaskInput.value + "<br><br><br>";
+    cardTitle.contentEditable ="true";
     cardTitle.appendChild(editTaskSpan);
     addTaskInput.value = "";
     mySectionCard.appendChild(cardTitle);
@@ -32,6 +41,66 @@ createBtn.addEventListener("click", () => {
     savingDoingData()
     savingDoneData()
 });
+
+// DRAGGING A CARD AROUND
+
+myMain.addEventListener("dragstart" ,(action) => {
+    action.dataTransfer.setData("text",action.target.id);
+       savingToDoData()
+        savingDoingData()
+        savingDoneData()
+})
+
+toDo.addEventListener("dragover", (action) =>{
+    action.preventDefault();
+       savingToDoData()
+        savingDoingData()
+        savingDoneData()
+})
+
+toDo.addEventListener("drop", (action) =>{
+    const targetData = action.dataTransfer.getData("text");
+    action.preventDefault();
+    action.target.appendChild(document.getElementById(targetData))
+   savingToDoData()
+        savingDoingData()
+        savingDoneData()
+})
+
+doing.addEventListener("dragover", (action) =>{
+    action.preventDefault();
+       savingToDoData()
+        savingDoingData()
+        savingDoneData()
+})
+
+doing.addEventListener("drop", (action) =>{
+    const targetData = action.dataTransfer.getData("text");
+    action.preventDefault();
+    action.target.appendChild(document.getElementById(targetData));
+       savingToDoData()
+        savingDoingData()
+        savingDoneData()
+})
+
+done.addEventListener("dragover", (action) =>{
+    action.preventDefault();
+       savingToDoData()
+        savingDoingData()
+        savingDoneData()
+})
+
+done.addEventListener("drop", (action) =>{
+    const targetData = action.dataTransfer.getData("text");
+    action.preventDefault();
+    action.target.appendChild(document.getElementById(targetData));
+       savingToDoData()
+        savingDoingData()
+        savingDoneData()
+})
+
+
+
 
 // VIEW FULL CARD
 
@@ -70,51 +139,51 @@ myMain.addEventListener("change", (action) => {
 
 // MOVING A CARD TO TODO
 
-myMain.addEventListener("click", (action) => {
-    if (action.target.tagName === "H6") {
-        let content = action.target.parentElement.innerHTML;
-        let mySectionCard = document.createElement("section");
-        console.log(action.target)
-        mySectionCard.innerHTML = content;
-        toDo.appendChild(mySectionCard);
-        action.target.parentElement.remove();
-        savingToDoData()
-        savingDoingData()
-        savingDoneData()
-    }
-})
+// myMain.addEventListener("click", (action) => {
+//     if (action.target.tagName === "H6") {
+//         let content = action.target.parentElement.innerHTML;
+//         let mySectionCard = document.createElement("section");
+//         console.log(action.target)
+//         mySectionCard.innerHTML = content;
+//         toDo.appendChild(mySectionCard);
+//         action.target.parentElement.remove();
+//         savingToDoData()
+//         savingDoingData()
+//         savingDoneData()
+//     }
+// })
 
 // MOVING A CARD TO DOING
 
-myMain.addEventListener("click", (action) => {
-    if (action.target.tagName === "H3") {
-        let content = action.target.parentElement.innerHTML;
-        let mySectionCard = document.createElement("section");
-        // action.target.remove();
-        mySectionCard.innerHTML = content;
-        doing.appendChild(mySectionCard);
-        action.target.parentElement.remove();
-        savingToDoData()
-        savingDoingData()
-        savingDoneData()
-    }
-})
+// myMain.addEventListener("click", (action) => {
+//     if (action.target.tagName === "H3") {
+//         let content = action.target.parentElement.innerHTML;
+//         let mySectionCard = document.createElement("section");
+//         // action.target.remove();
+//         mySectionCard.innerHTML = content;
+//         doing.appendChild(mySectionCard);
+//         action.target.parentElement.remove();
+//         savingToDoData()
+//         savingDoingData()
+//         savingDoneData()
+//     }
+// })
 
 // MOVING A CARD TO DONE
 
-myMain.addEventListener("click", (action) => {
-    if (action.target.tagName === "H4") {
-        let content = action.target.parentElement.innerHTML;
-        let mySectionCard = document.createElement("section");
-        action.target.style.innerHTML = "<h6>Move to To-Do</h6>";
-        mySectionCard.innerHTML = content;
-        done.appendChild(mySectionCard);
-        action.target.parentElement.remove();
-        savingToDoData()
-        savingDoingData()
-        savingDoneData()
-    }
-})
+// myMain.addEventListener("click", (action) => {
+//     if (action.target.tagName === "H4") {
+//         let content = action.target.parentElement.innerHTML;
+//         let mySectionCard = document.createElement("section");
+//         action.target.style.innerHTML = "<h6>Move to To-Do</h6>";
+//         mySectionCard.innerHTML = content;
+//         done.appendChild(mySectionCard);
+//         action.target.parentElement.remove();
+//         savingToDoData()
+//         savingDoingData()
+//         savingDoneData()
+//     }
+// })
 
 // MARKING A TASKAS DONE AND UNDONE
 
@@ -192,8 +261,6 @@ gettingDoneData()
 // showSavedTasks();
 
 
-// CHANGE MARK AS DONE FROM TOGGLE TO ADD AND REMOVE
-// USE AN IF STATEMENT TO MAKE THE MARK AS DONE sTAY WhEN YOU MOVE A CARD
 // WORK ON RESPONSIVENESS
 // THEN MAKE THE ACTUAL TRELLO DESIGN CAUSE WHAT YOU CURRENTL HAVE IS PURE SHIT!!!!!!!!!
 // USE THE LIST CONTAINER AND ALL TO EDIT UR TRELLO CARD LIST
